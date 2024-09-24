7b13f029162c764be07ff7d35d22d1d84002f0c4
Kannanps96 <84639295+Kannanps96@users.noreply.github.com>
2024-09-24 11:06:50 +0530

    Update SecurityConfiguration.java

--- src/main/java/com/user/userregistration/config/SecurityConfiguration.java
+++ src/main/java/com/user/userregistration/config/SecurityConfiguration.java
@@ -21,6 +21,7 @@ import org.springframework.security.oauth2.server.resource.authentication.JwtAut
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
